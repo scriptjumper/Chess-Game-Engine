0c188154c227dbe997109e988c9ee63bd3525e39
Shaeen Singh <shaeendev@gmail.com>
2022-04-20 13:03:50 +0200

    Pawn Class

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -30,5 +30,6 @@
 - [Bishop Class](https://en.wikipedia.org/wiki/Bishop_(chess))
 - [Rook Class](https://en.wikipedia.org/wiki/Rook_(chess))
 - [Queen Class](https://en.wikipedia.org/wiki/Queen_(chess))
+- [Pawn Class](https://en.wikipedia.org/wiki/Pawn_(chess))
 
 <!-- [0.1.0]: https://github.com/scriptjumper/Java-Chess-Engine/releases/tag/v-0.1.0 -->
--- src/com/chess/engine/pieces/Piece.java
+++ src/com/chess/engine/pieces/Piece.java
@@ -10,14 +10,20 @@ public abstract class Piece {
 
     protected final int piecePosition;
     protected final Alliance pieceAlliance;
+    protected final boolean isFirstMove;
 
     Piece(final int piecePosition, final Alliance pieceAlliance) {
         this.piecePosition = piecePosition;
         this.pieceAlliance = pieceAlliance;
+        this.isFirstMove = false; // TODO: more work here!
     }
 
     public Alliance getPieceAlliance() {
         return this.pieceAlliance;
+    }
+
+    public boolean isFirstMove() {
+        return this.isFirstMove;
     }
 
     public abstract Collection<Move> calculateLegalMoves(final Board board);
